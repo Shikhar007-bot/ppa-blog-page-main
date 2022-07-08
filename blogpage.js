5c9f902a7cee3ee946c7fc675fe534879e5eefc0
Shikhar Malik <79395058+Shikhar007-bot@users.noreply.github.com>
2022-07-09 00:10:33 +0530

    Add files via upload

---
+++ blogpage.js
@@ -0,0 +1,16 @@
+// import React from "react";
+import Footer from "../components/Footer";
+import LatestArticle from "../components/LatestArticles";
+import NavBar from "../components/NavBar";
+
+const home = () => {
+    return (
+        <>
+        <NavBar />
+      <LatestArticle />
+      <Footer />
+      </>
+    );
+};
+
+export default home;
